ddb53e75756a166fa6f91586fb10adef5c944ee0
Fabiano Bueno <buenofb@gmail.com>
2021-11-02 16:13:10 -0300

    Initial commit

---
+++ R/Soma.R
@@ -0,0 +1,16 @@
+
+
+#' Soma dois numeros
+#'
+#' recebe um numero x e outro y e retorna a soma de x e y
+#'
+#' @param x primeiro argumento
+#' @param y segundo argumento
+#'
+#' @return a soma de dois numeros
+#'
+#' @export
+
+soma <- function (x,y) {
+  x+y
+}
